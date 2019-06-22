fc35805372a04f534eaf34beae2ba948870746a7
andrew <andrew.torgesen@gmail.com>
2019-06-22 10:16:47 -0700

    add initial yaml and filesystems fuctions

--- include/utils/support.h
+++ include/utils/support.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <stdexcept>
+#include <stdio.h>
+#include <iostream>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <chrono>
+#include <string>
 #include <random>
-
+#include <experimental/filesystem>
+#include <boost/algorithm/string.hpp>
 #include <Eigen/Core>
+#include <yaml-cpp/yaml.h>
 
 namespace utils {
 
@@ -18,6 +27,139 @@ typedef Eigen::Matrix<double, 6, 6> Matrix6d;
 typedef Eigen::Matrix<double, 7, 7> Matrix7d;
 typedef Eigen::Matrix<double, 8, 8> Matrix8d;
 typedef Eigen::Matrix<double, 9, 9> Matrix9d;
+
+inline bool file_exists (const std::string& name) {
+  struct stat buffer;
+  return (stat (name.c_str(), &buffer) == 0);
+}
+
+template <typename T>
+bool get_yaml_node(const std::string key, const std::string filename, T& val, bool print_error = true)
+{
+  // Try to load the YAML file
+  YAML::Node node;
+  try
+  {
+    node = YAML::LoadFile(filename);
+  }
+  catch (...)
+  {
+    std::cout << "Failed to Read yaml file " << filename << std::endl;
+  }
+
+  // Throw error if unable to load a parameter
+  if (node[key])
+  {
+    val = node[key].as<T>();
+    return true;
+  }
+  else
+  {
+    if (print_error)
+    {
+      throw std::runtime_error("Unable to load " + key + " from " + filename);
+    }
+    return false;
+  }
+}
+template <typename Derived1>
+bool get_yaml_eigen(const std::string key, const std::string filename, Eigen::MatrixBase<Derived1>& val, bool print_error=true)
+{
+  YAML::Node node = YAML::LoadFile(filename);
+  std::vector<double> vec;
+  if (node[key])
+  {
+    vec = node[key].as<std::vector<double>>();
+    if (vec.size() == (val.rows() * val.cols()))
+    {
+      int k = 0;
+      for (int i = 0; i < val.rows(); i++)
+      {
+        for (int j = 0; j < val.cols(); j++)
+        {
+          val(i,j) = vec[k++];
+        }
+      }
+      return true;
+    }
+    else
+    {
+      throw std::runtime_error("Eigen Matrix Size does not match parameter size for " + key + " in " + filename +
+                               ". Requested " + std::to_string(Derived1::RowsAtCompileTime) + "x" + std::to_string(Derived1::ColsAtCompileTime) +
+                               ", Found " + std::to_string(vec.size()));
+      return false;
+    }
+  }
+  else if (print_error)
+  {
+    throw std::runtime_error("Unable to load " + key + " from " + filename);
+  }
+  return false;
+}
+
+template <typename Derived>
+bool get_yaml_diag(const std::string key, const std::string filename, Eigen::MatrixBase<Derived>& val, bool print_error=true)
+{
+  Matrix<typename Derived::Scalar, Derived::RowsAtCompileTime, 1> diag;
+  if (get_yaml_eigen(key, filename, diag, print_error))
+  {
+    val = diag.asDiagonal();
+    return true;
+  }
+  return false;
+}
+
+template <typename T>
+bool get_yaml_priority(const std::string key, const std::string file1, const std::string file2, T& val)
+{
+  if (get_yaml_node(key, file1, val, false))
+  {
+    return true;
+  }
+  else
+  {
+    return get_yaml_node(key, file2, val, true);
+  }
+}
+
+template <typename Derived1>
+bool get_yaml_priority_eigen(const std::string key, const std::string file1, const std::string file2, Eigen::MatrixBase<Derived1>& val)
+{
+  if (get_yaml_eigen(key, file1, val, false))
+  {
+    return true;
+  }
+  else
+  {
+    return get_yaml_eigen(key, file2, val, true);
+  }
+}
+
+inline bool createDirIfNotExist(const std::string& dir)
+{
+  if(!std::experimental::filesystem::exists(dir))
+    return std::experimental::filesystem::create_directory(dir);
+  else
+    return false;
+}
+
+inline std::vector<std::string> split(const std::string& s, const char* delimeter)
+{
+   std::vector<std::string> tokens;
+   std::string token;
+   std::istringstream tokenStream(s);
+   while (std::getline(tokenStream, token, delimeter[0]))
+   {
+      tokens.push_back(token);
+   }
+   return tokens;
+}
+
+inline std::string baseName(const std::string& path)
+{
+  std::string filename = split(path, "/").back();
+  return split(filename, ".")[0];
+}
 
 // LEGACY skew
 inline Eigen::Matrix3d skew(const Eigen::Vector3d v)
